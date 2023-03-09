7cb5925065617c3931c61aa65fc7f1225e11a17f
muhactive <muhactive@gmail.com>
2023-03-09 21:04:26 +0200

    add fix to function index (count)

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from newspaper.models import Topic, Redactor, Newspaper
+
 
 def index(request):
-    pass
+    count_topic = Topic.objects.count()
+    count_redactor = Redactor.objects.count()
+    count_newspaper = Newspaper.objects.count()
+
+    context = {
+        "count_topic": count_topic,
+        "count_redactor": count_redactor,
+        "count_newspaper": count_newspaper
+    }
+
+    return render(request, "newspaper/index.html", context=context)
