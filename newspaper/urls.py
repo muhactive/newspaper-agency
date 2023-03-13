5b1efb05cccbd2f4a15e59497f19384d33d0eb68
muhactive <muhactive@gmail.com>
2023-03-13 12:10:21 +0200

    add path(redactor-list) in urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from newspaper.views import index
+from newspaper.views import index, RedactorListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list"
+    )
 ]
 
 app_name = "newspaper"
