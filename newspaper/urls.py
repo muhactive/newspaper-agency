f105573c61b69c1e120fe5f70d49f5378b39f281
muhactive <muhactive@gmail.com>
2023-03-13 14:08:16 +0200

    add path (topic-list) in urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from newspaper.views import index, RedactorListView
+from newspaper.views import (
+    index,
+    RedactorListView,
+    TopicListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,6 +12,11 @@ urlpatterns = [
         "redactors/",
         RedactorListView.as_view(),
         name="redactor-list"
+    ),
+    path(
+        "topics",
+        TopicListView.as_view(),
+        name="topic-list"
     )
 ]
 
