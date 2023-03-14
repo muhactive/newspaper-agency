ade21ec98f35047f3a5a5b47d0412c4eeebbbb03
muhactive <muhactive@gmail.com>
2023-03-14 18:48:41 +0200

    add fix path(/accounts instead /ragistration)

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("newspaper.urls", namespace="newspaper")),
-    path("registration/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
