f2b04c071bcafb1e72f0e222892eb47b7273d3dc
muhactive <muhactive@gmail.com>
2023-03-21 18:17:30 +0200

    add to Tests/test_admin test_admin_page_fieldsets_has_years_of_experience

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -23,3 +23,7 @@ class AdminSiteTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, self.redactor.years_of_experience)
 
+    def test_admin_page_fieldsets_has_years_of_experience(self):
+        url = reverse("admin:newspaper_redactor_change", args={self.redactor.id})
+        response = self.client.get(url)
+        self.assertContains(response, self.redactor.years_of_experience)
