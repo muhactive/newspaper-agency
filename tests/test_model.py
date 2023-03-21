10ad32813618bb573254270d38eaca84ba14729e
muhactive <muhactive@gmail.com>
2023-03-21 17:32:51 +0200

    add to Tests def test_newspaper_str

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-
-from newspaper.models import Redactor, Topic
+from django.utils import timezone
+from newspaper.models import Redactor, Topic, Newspaper
 
 
 class TestModel(TestCase):
@@ -34,3 +35,22 @@ class TestModel(TestCase):
         topic = Topic.objects.create(topic="test")
         self.assertEqual(str(topic), f"{topic.topic}")
 
+    def test_newspaper_str(self) -> None:
+        self.user = Redactor.objects.create_user(
+            username="test_name",
+            password="12345testpassword"
+        )
+        self.topic = Topic.objects.create(topic="topic_test")
+        publishers = [self.user]
+        newspaper = Newspaper.objects.create(
+            title="www",
+            content="test content",
+            published_date=timezone.now,
+            topic=self.topic,
+        )
+        newspaper.publishers.set(publishers)
+        self.assertEqual(
+            str(newspaper),
+            f"{newspaper.publishers} wrote {newspaper.title} on"
+            f" ({newspaper.published_date.strftime('%d-%m-%Y %H:%M:%S')})"
+        )
